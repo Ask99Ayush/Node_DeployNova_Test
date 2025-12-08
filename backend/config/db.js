695630bec0419e939e69b2a78572f2a4dbfab93a
Ayush Rao Chaudhary <Ask99Ayush@gmail.com>
2025-12-09 03:35:38 +0530

    Connection Done

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,20 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/ems_test");
-    console.log("MongoDB Connected");
+    // Use environment variable (provided by docker-compose)
+    const MONGO_URL = process.env.MONGO_URL || "mongodb://ems-mongo:27017/employees";
+
+    await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB Connected:", MONGO_URL);
   } catch (err) {
-    console.error(err);
+    console.error("MongoDB Connection Error:", err);
+    process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
